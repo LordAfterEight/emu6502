7002c680e98f9ca37fd6d89b0f278693acecc070
LordAfterEight <elias@stettmayer.de>
2025-09-10 08:38:48 +0200

    Create opcodes.h

---
+++ src/opcodes.h
@@ -0,0 +1,11 @@
+const int OP_HALT = 0x0F;
+const int OP_NOOP = 0xEA;
+
+// A register
+const int OP_LOADA_IMM = 0xA9; // Immediate
+
+// X register
+const int OP_LOADX_IMM = 0xA2; // Immediate
+
+// Y register
+const int OP_LOADY_IMM = 0xA0; // Immediate
